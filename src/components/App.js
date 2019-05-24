9e7cf3c8d5d10a310fc22d8d618269939295e4c6
Kasmenai <kasmenai@gmail.com>
2019-05-24 17:20:54 +0300

    add BoardContainer to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import BoardContainer from './Board/BoardContainer'
 
 function App() {
-  return <div className="app">Kanban Board</div>
+  return (
+    <div className="app">
+      <BoardContainer />
+    </div>
+  )
 }
 
 export default App
