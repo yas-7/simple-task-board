0870e4395aeade51d6c99217d143080e5462f2f6
Kasmenai <kasmenai@gmail.com>
2019-05-26 11:35:43 +0300

    add MOVE_CARD to column reducer

--- src/reducers/columns.js
+++ src/reducers/columns.js
@@ -1,5 +1,4 @@
-import { ADD_COLUMN } from '../actions'
-import { ADD_CARD } from '../actions'
+import { ADD_COLUMN, ADD_CARD, MOVE_CARD } from '../actions'
 
 export default function columns(state = { byId: {}, allIds: [] }, action) {
   switch (action.type) {
@@ -23,6 +22,43 @@ export default function columns(state = { byId: {}, allIds: [] }, action) {
           [action.columnId]: {
             ...state.byId[action.columnId],
             cards: [...state.byId[action.columnId].cards, action.cardId],
+          },
+        },
+      }
+    case MOVE_CARD:
+      const { source, destination, draggableId } = action
+      const sourceColumn = state.byId[source.droppableId]
+      const destinationColumn = state.byId[destination.droppableId]
+      if (sourceColumn === destinationColumn) {
+        const newCardIds = sourceColumn.cards.slice()
+        newCardIds.splice(source.index, 1)
+        newCardIds.splice(destination.index, 0, draggableId)
+        return {
+          ...state,
+          byId: {
+            ...state.byId,
+            [sourceColumn.id]: {
+              ...sourceColumn,
+              cards: newCardIds,
+            },
+          },
+        }
+      }
+      const sourceCardIds = sourceColumn.cards.slice()
+      sourceCardIds.splice(source.index, 1)
+      const destinationCardIds = destinationColumn.cards.slice()
+      destinationCardIds.splice(destination.index, 0, draggableId)
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [sourceColumn.id]: {
+            ...sourceColumn,
+            cards: sourceCardIds,
+          },
+          [destinationColumn.id]: {
+            ...destinationColumn,
+            cards: destinationCardIds,
           },
         },
       }
