ba2b6c3168bd711be37af7b5afe6d100547ae0c6
Kasmenai <kasmenai@gmail.com>
2019-05-26 11:17:11 +0300

    add draggable to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 import './style.css'
 
-const Card = ({ card }) => <div className="card">{card.text}</div>
+const Card = ({ card, index }) => (
+  <Draggable draggableId={card.id} index={index}>
+    {provided => (
+      <div
+        className="card"
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+      >
+        {card.text}
+      </div>
+    )}
+  </Draggable>
+)
 
 export default Card
