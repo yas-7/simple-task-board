de6a07f94a4365ce93237a1ea71a4530d1253280
Kasmenai <kasmenai@gmail.com>
2019-05-26 11:26:50 +0300

    add DragDropContext to board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
 import ColumnContainer from '../Column/ColumnContainer'
 import ColumnAdder from '../Adder/ColumnAdder'
 import './style.css'
 
-const Board = ({ columnsId }) => (
+const Board = ({ columnsId, onDragEnd }) => (
   <div className="board">
     <div className="board__wrapper">
-      {columnsId.map(columnId => (
-        <ColumnContainer key={columnId} columnId={columnId} />
-      ))}
-      <ColumnAdder />
+      <DragDropContext onDragEnd={onDragEnd}>
+        {columnsId.map(columnId => (
+          <ColumnContainer key={columnId} columnId={columnId} />
+        ))}
+        <ColumnAdder />
+      </DragDropContext>
     </div>
   </div>
 )
--- src/components/Board/BoardContainer.js
+++ src/components/Board/BoardContainer.js
@@ -1,8 +1,30 @@
 import { connect } from 'react-redux'
 import Board from './Board'
+import { moveCard } from '../../actions'
+
+const onDragEnd = (result, dispatch) => {
+  const { destination, source, draggableId } = result
+  if (!destination) {
+    return
+  }
+  if (
+    destination.droppableId === source.droppableId &&
+    destination.index === source.index
+  ) {
+    return
+  }
+  dispatch(moveCard(source, destination, draggableId))
+}
 
 const mapStateToProps = state => ({
   columnsId: state.columns.allIds,
 })
 
-export default connect(mapStateToProps)(Board)
+const mapDispatchToProps = dispatch => ({
+  onDragEnd: result => onDragEnd(result, dispatch),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Board)
