48565b8a9251f140988e6cbb8713ed31e9e6a034
Kasmenai <kasmenai@gmail.com>
2019-05-26 11:18:19 +0300

    add droppable to column

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Droppable } from 'react-beautiful-dnd'
 import CardContainer from '../Card/CardContainer'
 import CardAdder from '../Adder/CardAdder'
 import './style.css'
@@ -7,11 +8,21 @@ const Column = ({ column }) => {
   return (
     <div className="column">
       <div className="column__title">{column.title}</div>
-      <div className="column__content">
-        {column.cards.map(cardId => (
-          <CardContainer key={cardId} cardId={cardId} />
-        ))}
-      </div>
+      <Droppable droppableId={String(column.id)}>
+        {provided => (
+          <div
+            className="column__content"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {column.cards.map((cardId, index) => (
+              <CardContainer key={cardId} cardId={cardId} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+
       <CardAdder columnId={column.id} />
     </div>
   )
--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -3,6 +3,7 @@ import Column from './Column'
 
 const mapStateToProps = (state, ownProps) => ({
   column: state.columns.byId[ownProps.columnId],
+  index: ownProps.index,
 })
 
 export default connect(mapStateToProps)(Column)
