8ed12208eeb292d55632c54fc369f24d422b4a6e
kasmenai <kasmenai@gmail.com>
2020-01-12 16:41:34 +0300

    Initial build subtree commit

---
+++ precache-manifest.bfe381f06970759917a36828706404c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b96ac5d8b75dab897ea9443783a1300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9add058872c6fe166163",
+    "url": "/static/css/main.945896c6.chunk.css"
+  },
+  {
+    "revision": "9e7f690fb1c6e302ed16",
+    "url": "/static/js/2.e0ff4844.chunk.js"
+  },
+  {
+    "revision": "9add058872c6fe166163",
+    "url": "/static/js/main.07033e97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "2746d8732d4276b2e4c077faf2bce8ce",
+    "url": "/static/media/background.2746d873.jpg"
+  },
+  {
+    "revision": "08995658a8a07b788bf480f86b07f28b",
+    "url": "/static/media/montserrat-bold-webfont.08995658.woff2"
+  },
+  {
+    "revision": "c61b0fa6f34d3984a007580cdccd9882",
+    "url": "/static/media/montserrat-bold-webfont.c61b0fa6.woff"
+  },
+  {
+    "revision": "51e46fdd1a45c2a0e747babe750d917d",
+    "url": "/static/media/montserrat-regular-webfont.51e46fdd.woff"
+  },
+  {
+    "revision": "eb8007927efd8b493c676d6f77e29531",
+    "url": "/static/media/montserrat-regular-webfont.eb800792.woff2"
+  }
+]);
