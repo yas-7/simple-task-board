fa18d897b354b481fe885be8bac8d1bf17abd58c
kasmenai <kasmenai@gmail.com>
2020-11-12 08:58:02 +0300

    Updates

---
+++ precache-manifest.d3042b04db29160f475a39383b30a9ee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a523d2cee396eb245d50ccd76fe0ec",
+    "url": "/simple-task-board/index.html"
+  },
+  {
+    "revision": "1362098f26e2e97818f7",
+    "url": "/simple-task-board/static/css/main.72b9fb96.chunk.css"
+  },
+  {
+    "revision": "2b22df3ab883b89074ec",
+    "url": "/simple-task-board/static/js/2.8c2326bb.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/simple-task-board/static/js/2.8c2326bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1362098f26e2e97818f7",
+    "url": "/simple-task-board/static/js/main.789802da.chunk.js"
+  },
+  {
+    "revision": "d7875ec7f477639ac7be",
+    "url": "/simple-task-board/static/js/runtime-main.501ebfad.js"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/simple-task-board/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/simple-task-board/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/simple-task-board/static/media/Vector.6cb6e5fc.svg"
+  },
+  {
+    "revision": "2746d8732d4276b2e4c077faf2bce8ce",
+    "url": "/simple-task-board/static/media/background.2746d873.jpg"
+  },
+  {
+    "revision": "bf167b14b4fd8460394f0260edd4e7dd",
+    "url": "/simple-task-board/static/media/background2x.bf167b14.jpg"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/simple-task-board/static/media/cross.5fe0db66.svg"
+  },
+  {
+    "revision": "08995658a8a07b788bf480f86b07f28b",
+    "url": "/simple-task-board/static/media/montserrat-bold-webfont.08995658.woff2"
+  },
+  {
+    "revision": "c61b0fa6f34d3984a007580cdccd9882",
+    "url": "/simple-task-board/static/media/montserrat-bold-webfont.c61b0fa6.woff"
+  },
+  {
+    "revision": "51e46fdd1a45c2a0e747babe750d917d",
+    "url": "/simple-task-board/static/media/montserrat-regular-webfont.51e46fdd.woff"
+  },
+  {
+    "revision": "eb8007927efd8b493c676d6f77e29531",
+    "url": "/simple-task-board/static/media/montserrat-regular-webfont.eb800792.woff2"
+  }
+]);
